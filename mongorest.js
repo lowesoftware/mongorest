6008bf3158f0d127a2caa2a95334e12c11bd8085
Alexander Lowe <alex@isnap.com>
2014-05-26 17:31:26 -0700

    Implemented full api, could use more detailed error handling

--- mongorest.js
+++ mongorest.js
@@ -1,29 +1,117 @@
 // private
 var mongoclient = require('mongodb').MongoClient
-
+var mongoconfig;
 
 // constructor
-function MongoRest(conn, db) {
-	this.conn = conn;
-	this.db = db;
+function MongoRest(config) {
+	mongoconfig = config;
 }
 
 // methods
-MongoRest.prototype.find() {
+MongoRest.prototype.find = function(collection, filter, callback) {
+	mongoclient.connect(mongoconfig, function(err, db) {
+		if(err) {
+			callback(err);
+			return;
+		}
+    	db.collection(collection).find(filter).toArray( function(err, result) {
+    		if(err) {
+    			callback(err);
+    			return;
+    		}
 
+    		callback(undefined, result);
+    	});
+	});
 }
 
-MongoRest.prototype.add() {
+MongoRest.prototype.insert = function(collection, obj, callback) {
+	mongoclient.connect(mongoconfig, function(err, db) {
+		if(err) {
+			callback(err);
+			return;
+		}
+    	db.collection(collection).insert(
+    		obj, 
+			{w:1}, 
+			function (err, result) {
+				if(err) {
+					callback(err);
+					return;
+				}
 
+				callback(undefined, result[0]);
+			}
+		);
+	});
 }
 
-MongoRest.prototype.update() {
+MongoRest.prototype.modify = function(collection, filter, values, callback) {
+	mongoclient.connect(mongoconfig, function(err, db) {
+		if(err) {
+			callback(err);
+			return;
+		}
+    	db.collection(collection).findAndModify(
+			filter, 
+			[], 
+			{$set: values},
+			{},
+			function (err, result) {
+				if(err) {
+					callback(err);
+					return;
+				}
 
+				callback(undefined, result);
+			}
+		);
+	});
 }
 
-MongoRest.prototype.remove() {
-	
+
+MongoRest.prototype.update = function(collection, filter, values, callback) {
+	mongoclient.connect(mongoconfig, function(err, db) {
+		if(err) {
+			callback(err);
+			return;
+		}
+    	db.collection(collection).findAndModify(
+			filter, 
+			[], 
+			values,
+			{},
+			function (err, result) {
+				if(err) {
+					callback(err);
+					return;
+				}
+
+				callback(undefined, result);
+			}
+		);
+	});
+}
+
+MongoRest.prototype.remove = function(collection, filter, callback) {
+	mongoclient.connect(mongoconfig, function(err, db) {
+		if(err) {
+			callback(err);
+			return;
+		}
+    	db.collection(collection).remove(
+			filter, 
+			function (err, result) {
+				if(err) {
+					callback(err);
+					return;
+				}
+
+				callback(undefined, result);
+			}
+		);
+	});
 }
 
 //export the class
-modules.exports = MongoRest;
+module.exports = MongoRest;
---
+++ server.js
@@ -0,0 +1,105 @@
+/*
+200 - OK
+404 - Not Found
+500 - Internal Server Error
+201 - Created
+304 - Not Modified
+400 - Bad Request
+401 - Unauthorized
+403 - Forbidden
+*/
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    MongoRest = require('./mongorest'),
+    ObjectID = require('mongodb').ObjectID;
+
+var app = express();
+
+app.use(bodyParser());
+
+mongorest = new MongoRest("mongodb://localhost:27017/mongorest");
+
+
+// get a document by id
+app.get('/:collection/:id', function(req, res) {
+	mongorest.find(req.params.collection, {_id:new ObjectID(req.params.id)}, function (err, result) {
+		if(err) {
+			res.json(500, err);
+			return;
+		}
+
+		if(result.length == 0) {
+			res.json(404, {});
+			return;
+		}
+
+		res.json(200, result[0]);
+	});
+    
+});
+
+
+// replace a document by id
+app.put('/:collection/:id', function(req, res) {
+	mongorest.update(req.params.collection, {_id:new ObjectID(req.params.id)}, req.body, function (err, result) {
+		if(err) {
+			res.json(500, err);
+			return;
+		}
+
+		res.json(200);
+	});
+    
+});
+
+
+// update a document by id
+app.post('/:collection/:id', function(req, res) {
+	mongorest.modify(req.params.collection, {_id:new ObjectID(req.params.id)}, req.body, function (err, result) {
+		if(err) {
+			res.json(500, err);
+			return;
+		}
+
+		res.json(200);
+	});
+    
+});
+
+
+
+// delete a document by id
+app.delete('/:collection/:id', function(req, res) {
+	mongorest.remove(req.params.collection, {_id:new ObjectID(req.params.id)}, function (err, result) {
+		if(err) {
+			res.json(500, err);
+			return;
+		}
+
+		if(result == 0) {
+
+		}
+
+		res.json(200);
+	});
+    
+});
+
+
+// create a document
+app.post('/:collection', function(req, res) {
+	mongorest.insert(req.params.collection, req.body, function (err, result) {
+		if(err) {
+			res.json(500, err);
+			return;
+		}
+
+		res.json(200, result);
+	});
+    
+});
+
+var port = Number(3030);
+app.listen(port);
+console.log('Listening on port ' + port + '...');
