e2437bc05f942cb628de6f260028e562c3a02d85
Alexander Lowe <alex@isnap.com>
2014-05-26 21:34:43 -0700

    Moved routes into the mongorest module so it's a nice off the shelf lib

--- mongorest.js
+++ mongorest.js
@@ -3,11 +3,118 @@ var mongoclient = require('mongodb').MongoClient
 var mongoconfig;
 
 // constructor
-function MongoRest(config) {
+function MongoRest(expressapp, config) {
 	mongoconfig = config;
+
+
+	// get a document by id
+	expressapp.get('/:collection/:id', function(req, res) {
+		mongorest.find(req.params.collection, {_id:new ObjectID(req.params.id)}, function (err, result) {
+			if(err) {
+				res.json(500, err);
+				return;
+			}
+
+			if(result.length == 0) {
+				res.json(404, {});
+				return;
+			}
+
+			res.json(200, result[0]);
+		});
+	    
+	});
+
+
+
+	// list all documents with an optional filter
+	expressapp.get('/:collection', function(req, res) {
+		if(!req.query.filter) {
+			req.query.filter = "{}"
+		};
+
+		mongorest.find(req.params.collection, JSON.parse(req.query.filter), function (err, result) {
+			if(err) {
+				res.json(500, err);
+				return;
+			}
+
+			if(result.length == 0) {
+				res.json(404, {});
+				return;
+			}
+
+			res.json(200, result);
+		});
+	    
+	});
+
+
+
+	// replace a document by id
+	expressapp.put('/:collection/:id', function(req, res) {
+		mongorest.update(req.params.collection, {_id:new ObjectID(req.params.id)}, req.body, function (err, result) {
+			if(err) {
+				res.json(500, err);
+				return;
+			}
+
+			res.json(200);
+		});
+	    
+	});
+
+
+	// update a document by id
+	expressapp.post('/:collection/:id', function(req, res) {
+		mongorest.modify(req.params.collection, {_id:new ObjectID(req.params.id)}, req.body, function (err, result) {
+			if(err) {
+				res.json(500, err);
+				return;
+			}
+
+			res.json(200);
+		});
+	    
+	});
+
+
+
+	// delete a document by id
+	expressapp.delete('/:collection/:id', function(req, res) {
+		mongorest.remove(req.params.collection, {_id:new ObjectID(req.params.id)}, function (err, result) {
+			if(err) {
+				res.json(500, err);
+				return;
+			}
+
+			if(result == 0) {
+
+			}
+
+			res.json(200);
+		});
+	    
+	});
+
+
+	// create a document
+	expressapp.post('/:collection', function(req, res) {
+		mongorest.insert(req.params.collection, req.body, function (err, result) {
+			if(err) {
+				res.json(500, err);
+				return;
+			}
+
+			res.json(200, result);
+		});
+	    
+	});
+
 }
 
-// methods
+
+// database methods
 MongoRest.prototype.find = function(collection, filter, callback) {
 	mongoclient.connect(mongoconfig, function(err, db) {
 		if(err) {
